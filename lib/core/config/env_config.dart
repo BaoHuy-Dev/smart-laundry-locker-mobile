9245ee6968e5adbcf459ab3eca2ee4f57364557a
BaoHuy-Dev <nqbhuy2004nt@gmail.com>
2026-06-16 17:22:31 +0700

    fix: update api base url to point to direct droplet IP

--- lib/core/config/env_config.dart
+++ lib/core/config/env_config.dart
@@ -6,7 +6,7 @@ part 'env_config.g.dart';
 abstract class EnvConfig {
   @EnviedField(
     varName: 'API_BASE_URL',
-    defaultValue: 'https://api-dev.aisl.io.vn',
+    defaultValue: 'http://146.190.84.136:8080',
     //'http://10.0.2.2:3000', // 10.0.2.2 is Android emulator's alias for host localhost
   )
   static const String apiBaseUrl = _EnvConfig.apiBaseUrl;
